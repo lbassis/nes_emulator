eef0b356ab785baff22cb73d7c7f6d3283e5f528
Lucas Assis <assislba@gmail.com>
2023-08-28 15:20:32 -0300

    adding the memory and reading the program into it

--- cpu.hpp
+++ cpu.hpp
@@ -8,7 +8,8 @@
 #define STATUS_NEGATIVE 1<<7
 #define STATUS_N_NEGATIVE 255-(1<<7)
 
-//#define 
+#define PC_INITIAL_VALUE_ADDRESS 0xFFFC
+#define PC_INITIAL_VALUE 0x8000
 
 using namespace std;
 using std::vector;
@@ -18,7 +19,8 @@ private:
   uint8_t accumulator;
   uint8_t reg_x;
   uint8_t status;
-  uint8_t pc;
+  uint16_t pc;
+  uint8_t memory[0xFFFF];
 
   inline void zero_pc() { this->pc = 0; }
   void set_zero_flag(bool flag) { flag ? this->status |= STATUS_ZERO : this->status &= STATUS_N_ZERO; }
@@ -30,9 +32,12 @@ private:
     (val & STATUS_NEGATIVE) ? set_negative_flag(true) : set_negative_flag(false);
   }
 
+  uint8_t mem_read(uint16_t address) { return memory[address]; }
+  void mem_write(uint16_t address, uint8_t data) { memory[address] = data; }
+  
   
 public:
-  cpu() : accumulator(0), status(0), pc(0) {}
+  cpu() : accumulator(0), status(0), pc(PC_INITIAL_VALUE) {}
   void print_status() { cout << bitset<8>(this->status) << endl; }
   inline uint8_t get_acc() { return this->accumulator; }
   inline uint8_t get_reg_x() { return this->reg_x; }
@@ -44,11 +49,17 @@ public:
   void set_acc(uint8_t val) { this->accumulator = val; }
   void set_reg_x(uint8_t val) { this->reg_x = val; }
 
-  void interpret(vector<uint8_t> program) {
-    zero_pc();
+  void reset() {
+    accumulator = 0;
+    reg_x = 0;
+    status = 0;
+    pc = mem_read_u16(PC_INITIAL_VALUE_ADDRESS);
+  }
+    
+  void run() {
     
     while (true) {
-      auto opcode = program[pc];
+      auto opcode = mem_read(pc);
       pc++;
 
       switch (opcode) {
@@ -56,7 +67,7 @@ public:
 	return;
 
       case 0xA9: { /* LDA */
-	auto param = program[pc];
+	auto param = mem_read(pc);
 	pc++;
 
 	accumulator = param;
@@ -82,4 +93,33 @@ public:
     }
   }
 
+  void load(vector<uint8_t> program) {
+    uint16_t program_offset = PC_INITIAL_VALUE;
+    mem_write_u16(PC_INITIAL_VALUE_ADDRESS, PC_INITIAL_VALUE);
+
+    for (int i = 0; i < program.size(); i++) {
+      memory[program_offset + i] = program[i];
+    }
+  }
+
+  inline void load_and_run(vector<uint8_t> program) {
+    load(program);
+    reset();
+    run();
+  }
+
+  uint16_t mem_read_u16(uint16_t address) {
+    uint8_t low = mem_read(address);
+    uint8_t high = mem_read(address+1);
+
+    return high << 8 | (uint16_t) low;
+  }
+
+  void mem_write_u16(uint16_t address, uint16_t data) {
+    uint8_t low = data & 0xff;
+    uint8_t high = (data >> 8) & 0xff;
+
+    mem_write(address, low);
+    mem_write(address+1, high);
+  }
 };
--- tests.cpp
+++ tests.cpp
@@ -8,7 +8,7 @@ void test_0xa9_lda(int &tests_made, int &tests_passed) {
   cpu cpu;
   vector<uint8_t> program = {0xa9, 0x05, 0x00};
 
-  cpu.interpret(program);
+  cpu.load_and_run(program);
   
   if (cpu.get_acc() == 0x05) { tests_passed++; }
   tests_made++;
@@ -24,7 +24,7 @@ void test_0xa9_lda_zero(int &tests_made, int &tests_passed) {
   cpu cpu;
   vector<uint8_t> program = {0xa9, 0x00, 0x00};
 
-  cpu.interpret(program);
+  cpu.load_and_run(program);
   
   if (cpu.get_acc() == 0x00) { tests_passed++; }
   tests_made++;
@@ -40,7 +40,7 @@ void test_0xa9_lda_negative(int &tests_made, int &tests_passed) {
   cpu cpu;
   vector<uint8_t> program = {0xa9, 0xFF, 0x00};
 
-  cpu.interpret(program);
+  cpu.load_and_run(program);
   
   if (cpu.get_acc() == 0xFF) { tests_passed++; }
   tests_made++;
@@ -57,7 +57,7 @@ void test_0xaa_n_zero(int &tests_made, int &tests_passed) {
   cpu cpu;
   vector<uint8_t> program = {0xa9, 0x05, 0xaa, 0x00};
 
-  cpu.interpret(program);
+  cpu.load_and_run(program);
   
   if (cpu.get_reg_x() == 0x05) { tests_passed++; }
   tests_made++;
@@ -72,41 +72,68 @@ void test_0xaa_n_zero(int &tests_made, int &tests_passed) {
 /* 0xE8 */
 void test_inx_overflow(int &tests_made, int &tests_passed) {
   cpu cpu;
-  vector<uint8_t> program = {0xe8, 0xe8, 0x00};
+  vector<uint8_t> program = {0xa9, 0xff, 0xaa, 0xe8, 0xe8, 0x00};
 
-  cpu.set_reg_x(0xff);
-  cpu.interpret(program);
+  cpu.load_and_run(program);
   
   if (cpu.get_reg_x() == 0x1) { tests_passed++; }
   tests_made++;
 }
-
 
 /* general */
 void test_first_ops(int &tests_made, int &tests_passed) {
   cpu cpu;
   vector<uint8_t> program = {0xa9, 0xc0, 0xaa, 0xe8, 0x00};
 
-  cpu.interpret(program);
+  cpu.load_and_run(program);
   
   if (cpu.get_reg_x() == 0xc1) { tests_passed++; }
   tests_made++;
 }
 
+/* memory */
+void read_8000(int &tests_made, int &tests_passed) {
+  cpu cpu;
+  vector<uint8_t> program = {0xa9, 0xc0};
+
+  cpu.load(program);
+
+  if (cpu.mem_read_u16(0x8000) == 0xc0a9) { tests_passed++; }
+  tests_made++;
+}
+
+void write_and_read(int &tests_made, int &tests_passed) {
+  cpu cpu;
+
+  cpu.mem_write_u16(0x8000, 0xc0a9);
+
+  if (cpu.mem_read_u16(0x8000) == 0xc0a9) { tests_passed++; }
+  tests_made++;
+}
+
+
 int main() {
 
   int tests_made = 0;
   int tests_passed = 0;
-  
+
+  cout << "Instruction tests:" << endl;
   test_0xa9_lda(tests_made, tests_passed);
   test_0xa9_lda_zero(tests_made, tests_passed);
   test_0xa9_lda_negative(tests_made, tests_passed);
 
   test_0xaa_n_zero(tests_made, tests_passed);
-
+  
   test_inx_overflow(tests_made, tests_passed);
-
+  
   test_first_ops(tests_made, tests_passed);
   
+  cout << "Passed " << tests_passed << "/" << tests_made << endl;
+
+  tests_made = tests_passed = 0;
+
+  cout << "Memory tests:" << endl;
+  read_8000(tests_made, tests_passed);
+  write_and_read(tests_made, tests_passed);
   cout << "Passed " << tests_passed << "/" << tests_made << endl;
 }
