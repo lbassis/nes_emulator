8b30b67ab299bb9dbe8fbc23621db849f48afc8d
Lucas Assis <assislba@gmail.com>
2023-09-12 15:59:20 -0300

    adapted makefile and tests main file to reflect the new tests structure

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 tests: 
-	g++ -o tests cpu_registers.cpp cpu_memory.cpp cpu_general.cpp tests.cpp
-	./tests
+	g++ -o tests/tests cpu_registers.cpp cpu_memory.cpp cpu_general.cpp tests/tests.cpp
+	./tests/tests
+
+verbose_tests:
+	g++ -o tests/tests cpu_registers.cpp cpu_memory.cpp cpu_general.cpp tests/tests.cpp -DVERBOSE
+	./tests/tests
 
 clean:
-	rm tests
+	rm ./tests/tests
 
-.PHONY: tests
+.PHONY: tests verbose_tests
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,116 +1,12 @@
-#include "cpu.h"
+#include "../cpu.h"
+
+#include "general.h"
+#include "memory.h"
+#include "lda.h"
+#include "tax.h"
+#include "inx.h"
 
 using namespace std;
-using std::vector;
-
-/* 0xA9 */
-void test_0xa9_lda(int &tests_made, int &tests_passed) {
-  cpu cpu;
-  vector<uint8_t> program = {0xa9, 0x05, 0x00};
-
-  cpu.load_and_run(program);
-  
-  if (cpu.get_acc() == 0x05) { tests_passed++; }
-  tests_made++;
-
-  if (cpu.get_zero() == false) { tests_passed++; }
-  tests_made++;
-
-  if (cpu.get_negative() == false) { tests_passed++; } 
-  tests_made++;
-}
-
-void test_0xa9_lda_zero(int &tests_made, int &tests_passed) {
-  cpu cpu;
-  vector<uint8_t> program = {0xa9, 0x00, 0x00};
-
-  cpu.load_and_run(program);
-  
-  if (cpu.get_acc() == 0x00) { tests_passed++; }
-  tests_made++;
-
-  if (cpu.get_zero() == true) { tests_passed++; }
-  tests_made++;
-
-  if (cpu.get_negative() == false) { tests_passed++; } 
-  tests_made++;
-}  
-
-void test_0xa9_lda_negative(int &tests_made, int &tests_passed) {
-  cpu cpu;
-  vector<uint8_t> program = {0xa9, 0xFF, 0x00};
-
-  cpu.load_and_run(program);
-  
-  if (cpu.get_acc() == 0xFF) { tests_passed++; }
-  tests_made++;
-
-  if (cpu.get_zero() == false) { tests_passed++; }
-  tests_made++;
-
-  if (cpu.get_negative() == true) { tests_passed++; } 
-  tests_made++;
-}
-
-/* 0xAA */
-void test_0xaa_n_zero(int &tests_made, int &tests_passed) {
-  cpu cpu;
-  vector<uint8_t> program = {0xa9, 0x05, 0xaa, 0x00};
-
-  cpu.load_and_run(program);
-  
-  if (cpu.get_reg_x() == 0x05) { tests_passed++; }
-  tests_made++;
-
-  if (cpu.get_zero() == false) { tests_passed++; }
-  tests_made++;
-
-  if (cpu.get_negative() == false) { tests_passed++; } 
-  tests_made++;
-}
-
-/* 0xE8 */
-void test_inx_overflow(int &tests_made, int &tests_passed) {
-  cpu cpu;
-  vector<uint8_t> program = {0xa9, 0xff, 0xaa, 0xe8, 0xe8, 0x00};
-
-  cpu.load_and_run(program);
-  
-  if (cpu.get_reg_x() == 0x1) { tests_passed++; }
-  tests_made++;
-}
-
-/* general */
-void test_first_ops(int &tests_made, int &tests_passed) {
-  cpu cpu;
-  vector<uint8_t> program = {0xa9, 0xc0, 0xaa, 0xe8, 0x00};
-
-  cpu.load_and_run(program);
-  
-  if (cpu.get_reg_x() == 0xc1) { tests_passed++; }
-  tests_made++;
-}
-
-/* memory */
-void read_8000(int &tests_made, int &tests_passed) {
-  cpu cpu;
-  vector<uint8_t> program = {0xa9, 0xc0};
-
-  cpu.load(program);
-
-  if (cpu.mem_read_u16(0x8000) == 0xc0a9) { tests_passed++; }
-  tests_made++;
-}
-
-void write_and_read(int &tests_made, int &tests_passed) {
-  cpu cpu;
-
-  cpu.mem_write_u16(0x8000, 0xc0a9);
-
-  if (cpu.mem_read_u16(0x8000) == 0xc0a9) { tests_passed++; }
-  tests_made++;
-}
-
 
 int main() {
 
